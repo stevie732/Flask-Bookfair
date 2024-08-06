c3a4f323ce0abf026650640980a8ef3b444ccf9e
Stephen <stephenprahljr@outlook.com>
2024-08-05 20:06:29 -0400

    'fixed but no token displayed issue'

--- app/site/routes.py
+++ app/site/routes.py
@@ -7,7 +7,6 @@ site = Blueprint('site', __name__, template_folder='site_templates')
 def home():
     return render_template('index.html')
 
-@site.route('/profile/')
 @site.route('/profile')
 def profile():
     return render_template('profile.html')
